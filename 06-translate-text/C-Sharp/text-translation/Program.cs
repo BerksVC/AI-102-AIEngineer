ace6204e132c2e43acffdda6e84f63b7e130b58a
BerksVC <kirbyveracruz@outlook.com>
2022-06-21 14:32:48 +0800

    C# Added code on comment
    Added code on Detect Language instructions

--- 06-translate-text/C-Sharp/text-translation/Program.cs
+++ 06-translate-text/C-Sharp/text-translation/Program.cs
@@ -77,6 +77,30 @@ namespace translate_text
             string translation = "";
 
             // Use the Translator translate function
+            object[] body = new object[] { new { Text = text } };
+            var requestBody = JsonConvert.SerializeObject(body);
+            using (var client = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage())
+                {
+                    // Build the request
+                    string path = "/detect?api-version=3.0";
+                    request.Method = HttpMethod.Post;
+                    request.RequestUri = new Uri(translatorEndpoint + path);
+                    request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                    request.Headers.Add("Ocp-Apim-Subscription-Key", cogSvcKey);
+                    request.Headers.Add("Ocp-Apim-Subscription-Region", cogSvcRegion);
+
+                    // Send the request and get response
+                    HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                    // Read response as a string
+                    string responseContent = await response.Content.ReadAsStringAsync();
+
+                    // Parse JSON array and get language
+                    JArray jsonResponse = JArray.Parse(responseContent);
+                    language = (string)jsonResponse[0]["language"]; 
+                }
+            }
 
 
             // Return the translation
