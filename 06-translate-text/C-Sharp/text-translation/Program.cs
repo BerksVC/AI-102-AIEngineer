31c5b3f79a3c102a17ac11f7e96a1132e2f03573
BerksVC <kirbyveracruz@outlook.com>
2022-06-22 13:55:11 +0800

    Added Correct Code
    Added correct code on the indicated functions from the instructions

--- 06-translate-text/C-Sharp/text-translation/Program.cs
+++ 06-translate-text/C-Sharp/text-translation/Program.cs
@@ -66,17 +66,6 @@ namespace translate_text
             string language = "en";
 
             // Use the Translator detect function
-
-
-            // return the language
-            return language;
-        }
-
-        static async Task<string> Translate(string text, string sourceLanguage)
-        {
-            string translation = "";
-
-            // Use the Translator translate function
             object[] body = new object[] { new { Text = text } };
             var requestBody = JsonConvert.SerializeObject(body);
             using (var client = new HttpClient())
@@ -101,6 +90,41 @@ namespace translate_text
                     language = (string)jsonResponse[0]["language"]; 
                 }
             }
+
+            // return the language
+            return language;
+        }
+
+        static async Task<string> Translate(string text, string sourceLanguage)
+        {
+            string translation = "";
+
+            // Use the Translator translate function
+            object[] body = new object[] { new { Text = text } };
+            var requestBody = JsonConvert.SerializeObject(body);
+            using (var client = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage())
+                {
+                    // Build the request
+                    string path = "/translate?api-version=3.0&from=" + sourceLanguage + "&to=en" ;
+                    request.Method = HttpMethod.Post;
+                    request.RequestUri = new Uri(translatorEndpoint + path);
+                    request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                    request.Headers.Add("Ocp-Apim-Subscription-Key", cogSvcKey);
+                    request.Headers.Add("Ocp-Apim-Subscription-Region", cogSvcRegion);
+
+                    // Send the request and get response
+                    HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                    // Read response as a string
+                    string responseContent = await response.Content.ReadAsStringAsync();
+
+                    // Parse JSON array and get translation
+                    JArray jsonResponse = JArray.Parse(responseContent);
+                    translation = (string)jsonResponse[0]["translations"][0]["text"];  
+                }
+            }
+
 
 
             // Return the translation
